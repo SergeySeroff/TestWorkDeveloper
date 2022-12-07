5eff5d31000b91fd9974f263cedf71a583199876
SergeySeroff <harek20@mail.ru>
2022-12-07 14:16:25 +0300

    Exemple 1.0

---
+++ Exemple/Program.cs
@@ -0,0 +1,26 @@
+﻿
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] mass1 = new string[4] {"htllo", "2", "world", ":-)"};
+string[] mass2 = new string[mass1.Length];
+SecondArrayWithIF(mass1, mass2);
+PrintArray(mass2);
